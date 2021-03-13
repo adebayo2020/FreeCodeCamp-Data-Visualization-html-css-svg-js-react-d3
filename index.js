5765a69d5d5ffcc964a4bda0663e1e493518ff4b
adebayo2020 <ajibadeadebayoengr@gmail.com>
2021-03-13 06:08:35 +0100

    Let's make a face Part II

---
+++ index.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// // class App extends React.Component {
+// //   render() {
+// //     return <h1>hello JSX</h1>;
+// //   }
+// // }
+// console.log("ReactDOM", ReactDOM);
+// const App = () =><h1>hello JSX</h1>;
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   //   <StrictMode>
+//   <App />,
+//   //   </StrictMode>
+//   rootElement
+// );
