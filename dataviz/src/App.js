e39ae2a6deea619d14fa025d54c39558a7b48004
adebayo2020 <ajibadeadebayoengr@gmail.com>
2021-04-04 17:34:13 +0100

    Barchat without margin convention

--- dataviz/src/App.js
+++ dataviz/src/App.js
@@ -1,45 +1,46 @@
 import { useState, useEffect } from "react";
 import * as d3 from "d3";
+const { csv, scaleBand, scaleLinear, max } = d3;
 
 const csvUrl =
-  "https://gist.githubusercontent.com/duplebado/8996feb7aee2568e7ad2f38dbd2c7f1f/raw/cssNamedColors.csv";
+  "https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv";
 
 const width = 960;
 const height = 580;
 
-const centerX = width / 2;
-const centerY = height / 2;
-
-const pieArc = d3.arc().innerRadius(0).outerRadius(width);
-
-const colorPie = d3.pie().value(1);
 const App = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    d3.csv(csvUrl).then(setData);
+    const row = (d) => {
+      d.Population = +d["2020"];
+      return d;
+    };
+    csv(csvUrl, row).then((data) => {
+      setData(data.slice(0, 10));
+    });
   }, []);
 
   if (!data) {
     return <pre>Loading</pre>;
   }
+  const yScale = scaleBand()
+    .domain(data.map((d) => d.Country))
+    .range([0, height]);
+  const xScale = scaleLinear()
+    .domain([0, max(data, (d) => d.Population)])
+    .range([0, width]);
   return (
     <svg width={width} height={height}>
-      <g transform={`translate(${centerX}, ${centerY})`}>
-        {colorPie(data).map((d) => (
-          <path fill={d.data["RGB hex value"]} d={pieArc(d)} />
-        ))}
-      </g>
+      {data.map((d) => (
+        <rect
+          x={0}
+          y={yScale(d.Country)}
+          width={xScale(d.Population)}
+          height={yScale.bandwidth()}
+        />
+      ))}
     </svg>
   );
 };
-// {data.map((d, i) => (
-//   <path
-//     fill={d["RGB hex value"]}
-//     d={pieArc({
-//       startAngle: (i / data.length) * 2 * Math.PI,
-//       endAngle: ((i + 1) / data.length) * 2 * Math.PI,
-//     })}
-//   />
-// ))}
 export default App;
